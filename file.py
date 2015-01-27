f39fe7b2db8c56558933572e7346e4f30eb11e50
alexeal90 <alexeal90@gmail.com>
2015-01-27 21:03:31 +0100

    primer diccionario

--- file.py
+++ file.py
@@ -3,17 +3,17 @@
 
 fich=open("/etc/passwd","r")
 user_num=fich.readlines()
-print "El numero de usuarios de este ordenador es: " + str(len(user_num))
+print "El numero de usuarios en este ordenador es: " + str(len(user_num))
 fich.close()
 
 fich=open("/etc/passwd","r")
 
 for user in user_num:
     
-    conf = user.split(":");
-    username = conf[0]
-    shell = conf[-1]
-    print "El usuario: *" + username + "* utiliza la shell: " + shell
+	conf = user.split(":");
+	username = conf[0]
+	shell = conf[-1]
+	print "El usuario: *" + username + "* utiliza la shell: " + shell
 
 fich.close()
 
